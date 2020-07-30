302fdc279aa437eae046607b3a16b7f400477225
Fredilla Prima Chandradiva <chandradiva@Fredillas-MacBook-Pro-2.local>
2020-07-30 21:08:28 +0700

    create development service

---
+++ src/main/java/com/lynknow/api/controller/DevelopmentController.java
@@ -0,0 +1,27 @@
+package com.lynknow.api.controller;
+
+import com.lynknow.api.service.DevelopmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "developments")
+public class DevelopmentController {
+
+    @Autowired
+    private DevelopmentService developmentService;
+
+    @PostMapping("set-premium")
+    public ResponseEntity setToPremium() {
+        return developmentService.setToPremium();
+    }
+
+    @PostMapping("set-basic")
+    public ResponseEntity setToBasic() {
+        return developmentService.setToBasic();
+    }
+
+}
---
+++ src/main/java/com/lynknow/api/service/DevelopmentService.java
@@ -0,0 +1,10 @@
+package com.lynknow.api.service;
+
+import org.springframework.http.ResponseEntity;
+
+public interface DevelopmentService {
+
+    ResponseEntity setToPremium();
+    ResponseEntity setToBasic();
+
+}
