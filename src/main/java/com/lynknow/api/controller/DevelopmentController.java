9c80f1a355d1e631336a2462b4f68550a8c93426
Fredilla Prima Chandradiva <chandradiva@gmail.com>
2020-09-16 23:49:35 +0700

    testing stripe

--- src/main/java/com/lynknow/api/controller/DevelopmentController.java
+++ src/main/java/com/lynknow/api/controller/DevelopmentController.java
@@ -1,11 +1,11 @@
 package com.lynknow.api.controller;
 
+import com.lynknow.api.pojo.request.StripeChargeRequest;
 import com.lynknow.api.service.DevelopmentService;
+import com.lynknow.api.service.StripeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "developments")
@@ -13,6 +13,9 @@ public class DevelopmentController {
 
     @Autowired
     private DevelopmentService developmentService;
+
+    @Autowired
+    private StripeService stripeService;
 
     @PostMapping("set-premium")
     public ResponseEntity setToPremium() {
@@ -27,6 +30,16 @@ public class DevelopmentController {
     @PostMapping("buy-verification-credit")
     public ResponseEntity buyVerificationCredit() {
         return developmentService.buyVerificationCredit();
+    }
+
+    @PostMapping("test-charge")
+    public ResponseEntity testCharge(@RequestBody StripeChargeRequest request) {
+        return stripeService.createStripeCharge(request);
+    }
+
+    @GetMapping("retrieve-charge")
+    public ResponseEntity retrieveCharge(@RequestParam String chargeId) {
+        return stripeService.retrieveCharge(chargeId);
     }
 
 }
